ed0dad7d4263646af85e1986d8d0be8eb03406c3
notoriousbiggie-code <62277883+notoriousbiggie-code@users.noreply.github.com>
2020-05-28 18:39:11 +0530

    Create funnelscripts.php

---
+++ funnelscripts.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Funnel Scripts</title>
+      <meta http-equiv = "refresh" content = "0; url = https://funnelscripts.com/reserve-your-spot" />
+   </head>
+   <body>
+      <p></p>
+   </body>
+</html>
